2af38844690d23d36d0abf6b2de046e3e08cf3d6
Nathaniel81 <nathanield356@gmail.com>
2022-06-03 05:10:52 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int s;
+	int n;
+
+	a = '0';
+	b = '0';
+	c = ',';
+	s = ' ';
+	n = '\n';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		putchar(b);
+		b = b + 1;
+
+		if (b == ':')
+		{
+			b = '0';
+			a = a + 1;
+		}
+		if (a != ':' && b != ':')
+		{
+			putchar(c);
+			putchar(s);
+		}
+	}
+	putchar(n);
+	return (0);
+}
