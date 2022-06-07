812d11cb8049f83d44b73534e5ffe9bf3d81b4da
Nathaniel81 <nathanield356@gmail.com>
2022-06-08 02:11:23 +0300

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check for multiple of 3 & 5
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i = 1024;
+int j;
+int sum = 0;
+
+for (j = 0; j < i; j++)
+{
+if ((j % 3 == 0)||(j % 5 == 0))
+{
+sum = sum + j;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
