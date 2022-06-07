914f90980955dade238926b8925e4e39254c0366
Nathaniel81 <nathanield356@gmail.com>
2022-06-08 02:30:54 +0300

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,16 +5,13 @@
  */
 int main(void)
 {
-int i = 1024;
-int j;
+int i;
 int sum = 0;
 
-for (j = 0; j < i; j++)
+for (i = 0; i < 1024; i++)
 {
-if ((j % 3 == 0)||(j % 5 == 0))
-{
-sum = sum + j;
-}
+if ((i % 3) == 0) || (i % 5) == 0)
+sum += i;
 }
 printf("%d\n", sum);
 return (0);
