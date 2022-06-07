1759724b929272ca94312e4fd4c4b166e47d4964
Nathaniel81 <nathanield356@gmail.com>
2022-06-08 01:20:03 +0300

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * times_table - prints table
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+int i;
+int j;
+int k;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = i * j;
+if ((k / 10) == 0)
+{
+if (j == 0)
+{
+_putchar ('0');
+}
+if (j != 0)
+{
+_putchar (' ');
+_putchar ((k % 10) + '0');
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar (' ');
+}
+}
+else
+{
+_putchar ((k / 10) + '0');
+_putchar ((k % 10) + '0');
+if (j < 9)
+{
+_putchar (',');
+_putchar (' ');
+}
+}
+}
+_putchar ('\n');
+}
+}
