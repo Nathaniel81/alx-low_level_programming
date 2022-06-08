20ffde5b36f3ab8cb5a235bfb4879999d02cf299
Nathaniel81 <nathanield356@gmail.com>
2022-06-08 08:29:25 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && b > c)
+if (a > b && a > c)
 {
 	largest = a;
 }
@@ -22,13 +22,12 @@ else if (b > c && b > a)
 }
 else if (c > b)
 {
-       	largest = c;
+       largest = c;
 }
 else
 {
-	largest = c;
+	largest = b;
 }
 
 return (largest);
-
 }
