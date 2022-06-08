e78d8e91db637935acc6ce60215a12c461f3564a
Nathaniel81 <nathanield356@gmail.com>
2022-06-07 18:46:02 -0700

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
- * main = print first 50 fibonacci
- * Return: Always 0
+ * main - Entry point
+ * purpose - fibonacci
+ * Return: (0) always
  */
 int main(void)
 {
 long int i, x = 1, y = 2, sum = 0, fsum = 0;
 
-for (i = 0; i <= 48; i++)
-{
-if ((y % 2 == 0) && (y <= 4000000))
-{
-fsum = fsum + y;
-}
-sum = x + y;
-x = y;
-y = sum;
-}
-printf("%ld\n", fsum);
-return (0);
+	for (i = 0; i <= 48; i++)
+	{
+	if ((y % 2 == 0) && (y <= 4000000))
+	{
+	fsum = fsum + y;
+	}
+	sum = x + y;
+	x = y;
+	y = sum;
+	}
+	printf("%ld\n", fsum);
+	return (0);
 }
