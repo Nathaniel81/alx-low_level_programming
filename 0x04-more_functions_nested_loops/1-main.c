7f428844c7e8694c2dab52422bd1b1a8d4fbb21a
Nathaniel81 <nathanield356@gmail.com>
2022-06-09 01:48:21 -0700

    Task 1

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -7,14 +7,14 @@ int main(void)
 {
 char c;
 
-c = 'A';
+c = '0';
 
-printf("%c: %d\n", c, _isupper(c));
+printf("%c: %d\n", c, _isdigit(c));
 		     
 c = 'a';
 
 			 
-printf("%c: %d\n", c, _isupper(c));
+printf("%c: %d\n", c, _isdigit(c));
 
 return (0);
 
