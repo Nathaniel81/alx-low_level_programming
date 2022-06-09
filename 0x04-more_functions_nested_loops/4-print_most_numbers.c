8f8a2b7f0207134d2ef302ab7adb67b6918973c0
Nathaniel81 <nathanield356@gmail.com>
2022-06-09 04:51:45 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers without 2 and 4
+ *
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 58; n++)
+	{
+		if (n != 50 && n != 52)
+			_putchar(n);
+	}
+
