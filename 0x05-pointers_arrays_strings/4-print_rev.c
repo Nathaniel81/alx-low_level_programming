c0d543701a0fcab69240a7fcfe496fdb4ab7cb2b
Nathaniel81 <nathanield356@gmail.com>
2022-06-13 11:26:51 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: A pointer to a char.
+ * Return: 0 on sucess;
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		for  (i = i - 1 ; i >= 0; i--)
+		{
+			_putchar (s[i]);
+		}
+	_putchar(10);
+}
