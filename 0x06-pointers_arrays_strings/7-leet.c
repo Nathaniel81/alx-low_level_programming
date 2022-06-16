fb6b664d142b81d3ad1bd5f3a9161e2392b3a14e
Nathaniel81 <nathanield356@gmail.com>
2022-06-16 20:51:18 +0300

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: a pointer to char
+ * Return: *s
+ */
+char *leet(char *s)
+{
+	int x, y;
+	char o[] = "ol_ea__t";
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; o[y] != '\0'; y++)
+		{
+			if (s[x] == o[y] || s[x] == (o[y] - 32))
+			{
+				s[x] = y + '0';
+			}
+		}
+    }
+return (s);
+}
