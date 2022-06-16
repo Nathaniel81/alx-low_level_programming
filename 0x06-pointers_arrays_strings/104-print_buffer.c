dee6b7c398e123f40850e97885cdc0357d7e4866
Nathaniel81 <nathanield356@gmail.com>
2022-06-16 23:33:56 +0300

    Task 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_buffer - print the content of size bytes of the buffer
+ * @b: buffer addres
+ * @suze: number of characters
+ */
+void print_buffer(char *b, int size)
+{
+int x, y, l;
+
+if (size <= 0)
+printf("\n");
+else
+{
+for (x = 0; x < size; x += 10)
+{
+printf("%.8x:", x);
+for (y = x; y < x + 10; y++)
+{
+if (y % 2 == 0)
+printf(" ");
+if (y < size)
+printf("%.2x", *(b + y));
+else
+printf("  ");
+}
+printf(" ");
+for (l = x; l < x + 10; l++)
+{
+if (l >= size)
+break;
+if (*(b + l) < 32 || *(b + l) > 126)
+printf("%c", '.');
+else
+printf("%c", *(b + l));
+}
+printf("\n");
+}
+}
+}
