42e1e846ab968b341c9ed636258bb2fd4705ce18
Nathaniel81 <nathanield356@gmail.com>
2022-06-20 10:42:35 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,15 +7,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int i, j;
+	unsigned int i, j;
 
-for (i = 0; s[i] != '\0'; i++)
-{
-    for (j = 0; accept[j] != '\0'; j++)
-    {
-        if (s[i] == accept[j])
-        goto exit;
-    }
-}
-    exit: return (s[i] != '\0' ? s + i: '\0');
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				goto exit;
+			}
+		}
+	}
+
+exit:return (s[i] != '\0' ? s + i : '\0');
 }
