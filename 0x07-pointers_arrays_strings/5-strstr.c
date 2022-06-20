349af97be71348775ea34515ca7cbeaca2d335a7
Nathaniel81 <nathanield356@gmail.com>
2022-06-20 11:28:44 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,20 +7,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i;
+	int i;
 
-    while (*haystack != '\0')
-    {
-        i = 0;
-        while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
-        {
-            haystack++, needle++,i++;
-        }
-        if (*needle == '\0')
-        {
-            return (haystack - i);
-            haystack -= (i - 1), needle -= i;
-        }
-    }
-    return ('\0');
+	while (*haystack != '\0')
+	{
+		i = 0;
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+			haystack++;
+			needle++;
+			i++;
+		if (*needle == '\0')
+			return (haystack - i);
+		haystack -= (i - 1);
+		needle -= i;
+	}
+	return ('\0');
 }
