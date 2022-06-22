bf20ec01d5db5e89baade3dbac780587b9445b68
Nathaniel81 <nathanield356@gmail.com>
2022-06-22 15:06:43 -0700

    Task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings to check if they are identical
+ * @s1: string 1
+ * @s2: string 2, with wild card
+ * Return: 1 on success
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (!*s1 && !*s2)
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		{
+			return (0);
+		}
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+		{
+			return (1);
+		}
+		return (1);
+	}
+	return (0);
+}
