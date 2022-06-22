921ea153ec54df8b836ddaf8284eafe36307a13b
Nathaniel81 <nathanield356@gmail.com>
2022-06-22 07:25:22 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		return (1 + _strlen_recursion(&s[i + 1]));
+	}
+	return (0);
+}
