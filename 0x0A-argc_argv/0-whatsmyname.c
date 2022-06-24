523fcf81c08eefd1c8e8bdbeed133e3b43e4273b
Nathaniel81 <nathanield356@gmail.com>
2022-06-24 10:28:27 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 /**
  * main - entry point
- * @argc - counts the number of parameters
- * @argv - pointer
+ * @argc: counts the number of parameters
+ * @argv: pointer
  * Return: 0 on success
  */
-int main(int argc, char**argv)
+int main(int argc, char **argv)
 {
 	if (argc > 0)
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	return (0);
 }
