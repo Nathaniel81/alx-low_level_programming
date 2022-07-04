a5c41494ab7aa2e2ed504eef952f9b999dd4be6c
Nathaniel81 <nathanield356@gmail.com>
2022-07-04 12:19:42 -0700

    Oden Nash

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,61 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @s: string input
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(char *s)
+{
+	int i, len;
+	char *str;
+
+	if (s == NULL)
+		return (0);
+
+	for (len = 0; s[len]; len++)
+	;
+
+	str = malloc(sizeof(char) * len + 1);
+
+	if (str == 0)
+		return (0);
+
+	for (i = 0; i <= len; i++)
+		str[i] = s[i];
+
+	return (str);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: 1 0n success, -1 on error
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+
+	if (new_dog == 0 || name == 0 || owner == 0)
+		return (0);
+
+	new_dog->name = _strdup(name);
+	if (!new_dog->name)
+	{
+		free(new_dog);
+		return (0);
+	}
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+
+	if (!new_dog->owner)
+	{
+		free(new_dog);
+		free(new_dog->name);
+		return (0);
+	}
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,9 @@ typedef struct dog
 
 #ifndef MAIN_H
 #define MAIN_H
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
