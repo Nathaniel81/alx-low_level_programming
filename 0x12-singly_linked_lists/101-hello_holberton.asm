12957fa7290eb5c0f7ef9a7a6fed8def9d7cc939
Nathaniel81 <nathanield356@gmail.com>
2022-07-15 23:20:39 +0300

    Larry Wall

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,26 @@
-;
-
+;File: 101-hello_holberton.asm
+;Description: A 64-bit assembly program that printf Hello, Holberton.
 
 extern printf
 
 section .text
-
-
-section .text
-global main
+ global main
 
 
 main:
-; prints Hello, Holberton
+ push rbp
 
-push rbp
-mov rdi, format
-mov rsi, msg
-mov rax, 0
-call printf
+ mov rdi,fmt
+ mov rsi,msg
+ mov rax,0
+ call printf
 
-pop rbp
-max rax, 0
-ret
+ pop rbp
 
-section .rodata
-msg: db "Hello, Holberton", 0
-format: db "%s", 10, 0
+ mov rax, 0
+ ret
+
+section .data
+ msg: db "Hello, Holberton", 0
+ format: db "%s", 10, 0
+
