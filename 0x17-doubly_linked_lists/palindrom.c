da1f7829f615ee73c74efa67d0426ef1e469fc5d
Nathaniel81 <nathanield356@gmail.com>
2022-09-07 12:09:51 +0300

    Palindromes

---
+++ 0x17-doubly_linked_lists/palindrom.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int opposite(int n)
+{
+	int m = 0;
+
+	while (n > 0)
+	{
+		m = m * 10 + n % 10;
+		n = n / 10;
+	}
+	return m;
+}
+
+int main()
+{
+	int greatest = 0;
+
+	for (int i = 100; i <= 999; i++)
+	{
+		for (int j = 100; j <= 999; j++)
+		{
+			if (i * j == opposite (i * j) || i * j > greatest)
+				greatest = i * j;
+		}
+	}
+	printf("%d\n",greatest);
+
+	return 0;
+}
