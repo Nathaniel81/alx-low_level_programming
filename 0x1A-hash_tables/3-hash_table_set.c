f1778fbb7537a14794cadb75568c2fe852484145
Nathaniel81 <nathanield356@gmail.com>
2022-09-23 22:36:42 +0300

    key -> index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,11 +2,11 @@
 
 /**
  * hash_table_set - Adds an element to the hash table.
- * 
+ *
  * @ht: The hash table to add or update the key/value
  * @key: The key
  * @value: The hash table you want to add or update the key/value
- * 
+ *
  * Return: 1 if it succeeded, 0 otherwise
  */
 
@@ -14,6 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *head, *new_node;
 	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
+
 	head = ht->array[idx];
 	/*traverse = ht->array[idx];*/
 
@@ -33,9 +34,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		new_node->next = head;
 		head = new_node;
-		/*printf("%s\n", head->key);
+		/*
+		printf("%s\n", head->key);
 		printf("%s\n", head->value);
-		printf("%d\n", idx);*/
+		printf("%d\n", idx);
+		*/
 	}
 	else
 	{
