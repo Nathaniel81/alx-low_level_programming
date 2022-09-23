42e00fa3e38ab70831106c7fa18f546fb9528d8f
Nathaniel81 <nathanield356@gmail.com>
2022-09-23 23:53:50 +0300

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,28 +1,34 @@
 #include "hash_tables.h"
+#include <string.h>
+
+#define SUCCESS  1
+#define FAILURE  0
 
 /**
- * hash_table_set - Adds an element to the hash table.
+ * hash_table_set - Adds an element to the hash table. In case of collision,
+ * it will add an element at the beginning of  the list.
  *
- * @ht: The hash table to add or update the key/value
- * @key: The key
- * @value: The hash table you want to add or update the key/value
+ * @ht: The hash table you want to add or update the key/value to
+ * @key: The key, and cannot be empty string.
+ * @value: The value associated with the key, and can be empty string.
  *
  * Return: 1 if it succeeded, 0 otherwise
+ *
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node, *traverse;
-	unsigned long int idx;
+	unsigned long int i;
 	char *copy;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
+	if (ht == NULL || key == NULL || *key == 0 || value == NULL)
+		return (FAILURE);
 
-	idx =  key_index((const unsigned char *) key, ht->size);
-	traverse = ht->array[idx];
+	i =  key_index((const unsigned char *) key, ht->size);
+	traverse = ht->array[i];
 
-	while (traverse != NULL)
+	while (traverse)
 	{
 		if (strcmp(traverse->key, key) == 0)
 			break;
@@ -32,23 +38,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		new_node = malloc(sizeof(hash_node_t));
 		if (new_node == NULL)
-			return (0);
+			return (FAILURE);
 		new_node->key = strdup(key);
 		if (new_node->key == NULL)
-			return (0);
+			return (FAILURE);
 		new_node->value = strdup(value);
 		if (new_node->value == NULL)
-			return (0);
-		new_node->next = ht->array[idx];
-		ht->array[idx] = new_node;
+			return (FAILURE);
+		new_node->next = ht->array[i];
+		ht->array[i] = new_node;
 	}
 	else
 	{
 		copy = strdup(value);
 		if (copy == NULL)
-			return (0);
+			return (FAILURE);
 		free(traverse->value);
 		traverse->value = copy;
 	}
-	return (1);
+	return (SUCCESS);
 }
