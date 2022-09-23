3e8d0540b015b3c901f738f790f185a325bd87ad
Nathaniel81 <nathanield356@gmail.com>
2022-09-23 22:59:28 +0300

    key -> index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,8 +32,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node->value = strdup(value);
 		if (new_node->value == NULL)
 			return (0);
-		new_node->next = head;
-		head = new_node;
+		new_node->next = ht->array[idx];
+		ht->array[idx] = new_node;
 		/**
 		 * printf("%s\n", head->key);
 		 * printf("%s\n", head->value);
