8033f24a2ffedd39f9b14060a6a304b9911cf5e4
Nathaniel81 <nathanield356@gmail.com>
2022-09-24 00:58:11 +0300

    >>> ht['betty']

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,6 +15,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *traverse;
 	unsigned long int idx;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	idx = key_index((const unsigned char *)key, ht->size);
 	if (idx >= ht->size)
 		return (NULL);
