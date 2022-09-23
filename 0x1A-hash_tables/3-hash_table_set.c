34eaa2212bb8f9de87a1bc98ff3f8e103548d322
Nathaniel81 <nathanield356@gmail.com>
2022-09-23 22:48:05 +0300

    key -> index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,25 +13,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *head, *new_node;
-	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
-
-	head = ht->array[idx];
-	/*traverse = ht->array[idx];*/
+	unsigned long int idx;;
 
 	if (ht == NULL || key == NULL || value == NULL)
-		return (1);
+		return (0);
 
+	idx = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[idx];
+	/*traverse = ht->array[idx];*/
 	if (head == NULL)
 	{
 		new_node = malloc(sizeof(hash_node_t));
 		if (new_node == NULL)
-			return (1);
+			return (0);
 		new_node->key = strdup(key);
 		if (new_node->key == NULL)
-			return (1);
+			return (0);
 		new_node->value = strdup(value);
 		if (new_node->value == NULL)
-			return (1);
+			return (0);
 		new_node->next = head;
 		head = new_node;
 		/**
@@ -49,23 +49,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(head->value);
 				head->value = strdup(value);
 				if (head->value == NULL)
-					return (1);
-				return (0);
+					return (0);
+				return (1);
 			}
 			head = head->next;
 		}
 		new_node = malloc(sizeof(hash_node_t));
 		if (new_node == NULL)
-			return (1);
+			return (0);
 		new_node->key = strdup(key);
 		if (new_node->key == NULL)
-			return (1);
+			return (0);
 		new_node->value = strdup(value);
 		if (new_node->value == NULL)
-			return (1);
+			return (0);
 		new_node->next = head;
 		head = new_node;
 	}
 	/*printf("%s\n", head->value);*/
-	return (0);
+	return (1);
 }
