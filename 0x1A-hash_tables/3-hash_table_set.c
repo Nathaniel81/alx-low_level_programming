e153513bd4ad3915e5fdfed02f7ed2f8bc3233e7
Nathaniel81 <nathanield356@gmail.com>
2022-09-23 23:02:55 +0300

    key -> index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			if (strcmp(head->key, key) == 0)
 			{
-				/*free(head->value);*/
+				free(head->value);
 				head->value = strdup(value);
 				if (head->value == NULL)
 					return (0);
