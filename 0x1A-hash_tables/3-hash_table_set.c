f667ebd1957411da3b545ebfc602c87f600b6ee9
Nathaniel81 <nathanield356@gmail.com>
2023-01-13 10:24:20 +0300

    AdvTasks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,60 +1,41 @@
 #include "hash_tables.h"
-#include <string.h>
-
-#define SUCCESS  1
-#define FAILURE  0
 
 /**
- * hash_table_set - Adds an element to the hash table. In case of collision,
- * it will add an element at the beginning of  the list.
- *
- * @ht: The hash table you want to add or update the key/value to
- * @key: The key, and cannot be empty string.
- * @value: The value associated with the key, and can be empty string.
- *
+ * hash_table_set - adds an element to the hash table
+ * @ht: he hash table you want to add or update the
+ * key/value to
+ * @key: the key
+ * @value: value associated with the key
  * Return: 1 if it succeeded, 0 otherwise
- *
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *newNode, *temp;
-	unsigned long int i;
-	char *copy;
+	unsigned long int index, size;
+	hash_node_t *new;
 
-	if (ht == NULL || key == NULL || *key == 0 || value == NULL)
+	if (!ht || !value || !key)
+		return (0);
+	if (strlen(key) == 0)
+		return (0);
+	size = ht->size;
+	index = key_index((const unsigned char *) key, size);
+
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
 		return (0);
 
-	i =  key_index((const unsigned char *) key, ht->size);
-	temp = ht->array[i];
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-	while (temp)
-	{
-		if (strcmp(temp->key, key) == 0)
-			break;
-		temp = temp->next;
-	}
-	if (temp == NULL)
-	{
-		newNode = malloc(sizeof(hash_node_t));
-		if (newNode == NULL)
-			return (FAILURE);
-		newNode->key = strdup(key);
-		if (newNode->key == NULL)
-			return (FAILURE);
-		newNode->value = strdup(value);
-		if (newNode->value == NULL)
-			return (FAILURE);
-		newNode->next = ht->array[i];
-		ht->array[i] = newNode;
-	}
-	else
-	{
-		copy = strdup(value);
-		if (copy == NULL)
-			return (0);
-		free(temp->value);
-		temp->value = copy;
-	}
 	return (1);
 }
